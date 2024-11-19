f8165b3d863fa78c825a2552ab2a96d9db1e83b3
Rodolph <rodolphcorp@gmail.com>
2024-11-19 01:03:27 +0100

    test fistName

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1>Hello {{ fisrtNAme.toUpperCase() }}</h1>
+  <p>bien ou quoi ?</p>
+</template>
+
+<script setup>
+const fisrtNAme = "John";
+</script>
+
+<style>
+h1 {
+  color: red;
+}
+</style>
