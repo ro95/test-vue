1d683e431514895cce87d3edd547d89f9788489b
Rodolph <rodolphcorp@gmail.com>
2024-11-22 00:01:59 +0100

    key movie add

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,33 @@
 <template>
-  <p>Compteur : {{ count }}</p>
+  <p :style="{ active: count > 6 }">Compteur : {{ count }}</p>
   <div v-if="count > 6">Congrats vous avez cliqué plus de 6 fois</div>
   <div v-else>Vous avez cliqué moins de 6 fois</div>
-
   <button @click="increment">Increment</button>
   <button @click="decrement">Decrement</button>
+  <button @click="SortMovie">Réorganiser</button>
+  <ul>
+    <li v-for="movie in movies" :key="movie">
+      {{ movie }} <button @click="deleteMovie(movie)">Delete</button>
+    </li>
+  </ul>
 </template>
 
 <script setup>
 import { ref } from "vue";
 const count = ref(0);
+const movies = ref([
+  "The Shawshank Redemption",
+  "The Godfather",
+  "The Dark Knight",
+]);
+
 const increment = (event) => count.value++;
 const decrement = () => count.value--;
-</script>
 
-<style>
-h1 {
-  color: red;
-}
-</style>
+const deleteMovie = (movie) => {
+  movies.value = movies.value.filter((m) => m !== movie);
+};
+const SortMovie = () => {
+  movies.value.sort((a, b) => (a > b ? 1 : -1));
+};
+</script>
