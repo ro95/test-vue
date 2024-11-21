5c6711bfbfd6eddd7b05d15a1a1a9a7398ac5519
Rodolph <rodolphcorp@gmail.com>
2024-11-22 00:54:04 +0100

    exercice age dynamique

--- src/App.vue
+++ src/App.vue
@@ -1,44 +1,20 @@
 <template>
-  <p :style="{ active: count > 6 }">Compteur : {{ count }}</p>
-  <div v-if="count > 6">Congrats vous avez cliqué plus de 6 fois</div>
-  <div v-else>Vous avez cliqué moins de 6 fois</div>
-  <button @click="increment">Increment</button>
-  <button @click="decrement">Decrement</button>
-  <button @click="SortMovie">Réorganiser</button>
-  <form action="" @submit.prevent="addMovie">
-    <input type="text" placeholder="New film" v-model="movieName" />
-    <button>Ajouter</button>
-  </form>
-  <hr />
   <ul>
-    <li v-for="movie in movies" :key="movie">
-      {{ movie }} <button @click="deleteMovie(movie)">Delete</button>
-    </li>
+    <li>{{ person.firstname }}</li>
+    <li>{{ person.lastname }}</li>
+    <li>{{ person.age }}</li>
   </ul>
+  <button @click.prevent="randomAge">changer age</button>
 </template>
 
 <script setup>
 import { ref } from "vue";
-const count = ref(0);
-const movies = ref([
-  "The Shawshank Redemption",
-  "The Godfather",
-  "The Dark Knight",
-]);
-
-const increment = (event) => count.value++;
-const decrement = () => count.value--;
-
-const deleteMovie = (movie) => {
-  movies.value = movies.value.filter((m) => m !== movie);
-};
-const SortMovie = () => {
-  movies.value.sort((a, b) => (a > b ? 1 : -1));
-};
-
-const movieName = ref("");
-const addMovie = () => {
-  movies.value.push(movieName.value);
-  movieName.value = "";
+const person = ref({
+  firstname: "John",
+  lastname: "Doe",
+  age: 20,
+});
+const randomAge = () => {
+  person.value.age = Math.floor(Math.random() * 100);
 };
 </script>
